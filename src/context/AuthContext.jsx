528ac93adb124c7449a227987a88035fc4e70eb8
kaydan95 <kyori0515@gmail.com>
2023-01-09 15:20:06 +0900

    로그인 Auth 세팅
    AuthContext 구현, 로그인 후 이용 가능한 페이지 구현

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { createContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { onUserStateChange, login, logout } from "../api/firebase";
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      // console.log(user);
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, uid: user && user.uid, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
